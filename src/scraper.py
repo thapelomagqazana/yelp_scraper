e61cdf91412cff15bd1bd2c8565c8976a8874fb4
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-12-25 23:07:13 +0200

    Provided logging and Error Handling, also User-Agent Rotation

--- src/scraper.py
+++ src/scraper.py
@@ -1,42 +1,60 @@
-import requests
+import logging
+import urllib.request
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+# Configure logging
+logging.basicConfig(filename="scraper.log", level=logging.INFO)
 
 
-def scrape_yelp(search_query: str, location: str):
+def get_search_url(search_query: str, location: str) -> str:
     base_url = 'https://www.restaurants.co.za'
-    search_url = f"{base_url}/{search_query.lower()}/{location.lower()}"
-
-    response = requests.get(search_url)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    for result in soup.find_all("div", class_="laout_5 direcotry_listing"):
-        name = result.find('span', itemprop='name').get_text(strip=True)
-        print("Name of the Restaurant: " + name)
-        a_content = result.find("a", itemprop="url")
-        if a_content:
-            link = a_content["href"]
-            restaurant_response = requests.get(link)
-            restaurant_soup = BeautifulSoup(restaurant_response.text, "html.parser")
+    return f"{base_url}/{search_query.lower()}/{location.lower()}"
 
 
-            # Extract address and phone number from the restaurant page
-            address_element = restaurant_soup.find("li", class_="address")
-            phone_number_element = restaurant_soup.find("p", itemprop="telephone")
-            email_element = restaurant_soup.find('span', itemprop="email")
+def scrape_yelp(search_url: str):
+    try:
+        ua = UserAgent()
+        headers = {"User-Agent": ua.random}
 
-            address = address_element.get_text(strip=True) if address_element else "Address not available"
-            phone_number = phone_number_element.get_text(
-                strip=True) if phone_number_element else "Phone Number not available"
-            email = email_element.get_text(strip=True) if email_element else "Email Address not available"
-            print("Street Address: " + address)
-            print("Phone Number: " + phone_number)
-            print("Email Address: " + email)
-            print("-------------------")
+        req = urllib.request.Request(search_url, headers=headers)
+        html = urllib.request.urlopen(req).read()
+        soup = BeautifulSoup(html, "html.parser")
 
+        for result in soup.find_all("div", class_="laout_5 direcotry_listing"):
+            name = result.find('span', itemprop='name').get_text(strip=True)
+            logging.info("Name of the Restaurant: " + name)
+            a_content = result.find("a", itemprop="url")
+            if a_content:
+                link = a_content["href"]
+                restaurant_req = urllib.request.Request(link, headers=headers)
+                restaurant_html = urllib.request.urlopen(restaurant_req).read()
+                restaurant_soup = BeautifulSoup(restaurant_html, "html.parser")
 
+                # Extract address and phone number from the restaurant page
+                address_element = restaurant_soup.find("li", class_="address")
+                phone_number_element = restaurant_soup.find("p", itemprop="telephone")
+                email_element = restaurant_soup.find('span', itemprop="email")
+
+                address = address_element.get_text(strip=True) if address_element else "Address not available"
+                phone_number = phone_number_element.get_text(
+                    strip=True) if phone_number_element else "Phone Number not available"
+                email = email_element.get_text(strip=True) if email_element else "Email Address not available"
+                logging.info("Street Address: " + address)
+                logging.info("Phone Number: " + phone_number)
+                logging.info("Email Address: " + email)
+                logging.info("-------------------")
+
+    except urllib.error.HTTPError as e:
+        logging.error(f"HTTPError while scraping {search_url}: {e}")
+    except urllib.error.URLError as e:
+        logging.error(f"URLError while scraping {search_url}: {e}")
+    except Exception as e:
+        logging.error(f"Error scraping {search_url}: {e}")
 
 
 if __name__ == "__main__":
     search_query = 'gauteng'
     location = ''
-    scrape_yelp(search_query, location)
+    search_url = get_search_url(search_query, location)
+    scrape_yelp(search_url)
