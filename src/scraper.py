d0dc008c75964ac41110a7bda272b735e21ffaaa
thapelomagqazana <tapsmcgzee8@gmail.com>
2023-12-26 09:26:40 +0200

    Provided Asynchronous scraping to speed up the scraping process. To handle multiple requests concurrently without using threads

--- src/scraper.py
+++ src/scraper.py
@@ -2,23 +2,39 @@ import logging
 import urllib.request
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import aiohttp
+import asyncio
 
-# Configure logging
-logging.basicConfig(filename="scraper.log", level=logging.INFO)
+# Configure logging to both file and stdout
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] - %(message)s',
+    handlers=[
+        logging.FileHandler('scraper.log'),
+        logging.StreamHandler()  # This handler prints to the console
+    ]
+)
 
 
 def get_search_url(search_query: str, location: str) -> str:
     base_url = 'https://www.restaurants.co.za'
-    return f"{base_url}/{search_query.lower()}/{location.lower()}"
+    modified_search_query = search_query.lower().strip().replace(" ", "-")
+    modified_location = location.lower().strip().replace(" ", "-")
+
+    return f"{base_url}/{modified_search_query}/{modified_location}"
 
 
-def scrape_yelp(search_url: str):
+async def scrape_yelp(search_url: str, session):
     try:
         ua = UserAgent()
         headers = {"User-Agent": ua.random}
 
-        req = urllib.request.Request(search_url, headers=headers)
-        html = urllib.request.urlopen(req).read()
+        async with session.get(search_url, headers=headers) as response:
+            response.raise_for_status()
+            html = await response.text()
+
+        # req = urllib.request.Request(search_url, headers=headers)
+        # html = urllib.request.urlopen(req).read()
         soup = BeautifulSoup(html, "html.parser")
 
         for result in soup.find_all("div", class_="laout_5 direcotry_listing"):
@@ -27,8 +43,10 @@ def scrape_yelp(search_url: str):
             a_content = result.find("a", itemprop="url")
             if a_content:
                 link = a_content["href"]
-                restaurant_req = urllib.request.Request(link, headers=headers)
-                restaurant_html = urllib.request.urlopen(restaurant_req).read()
+                async with session.get(link, headers=headers) as rest_response:
+                    rest_response.raise_for_status()
+                    restaurant_html = await rest_response.text()
+
                 restaurant_soup = BeautifulSoup(restaurant_html, "html.parser")
 
                 # Extract address and phone number from the restaurant page
@@ -52,9 +70,16 @@ def scrape_yelp(search_url: str):
     except Exception as e:
         logging.error(f"Error scraping {search_url}: {e}")
 
+async def main():
+    restaurant_urls = [
+        get_search_url("western cape", ""),
+        get_search_url("gauteng", ""),
+
+    ]
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [scrape_yelp(url, session) for url in restaurant_urls]
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
-    search_query = 'gauteng'
-    location = ''
-    search_url = get_search_url(search_query, location)
-    scrape_yelp(search_url)
+    asyncio.run(main())
